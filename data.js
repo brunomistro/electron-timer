3de230c7c91743e828ae039a87010fe573752453
Bruno Mistro <34082572+brunomistro@users.noreply.github.com>
2022-03-07 14:50:33 -0300

    Done

--- data.js
+++ data.js
@@ -1,20 +1,34 @@
 const jsonfile = require('jsonfile-promised');
-const fs = require('fs')
+const fs = require('fs');
+const { log } = require('console');
 
 module.exports = {
 	salvaDados(curso, tempoEstudado){
 		let arquivoDoCurso = __dirname + "/data/" + curso + ".json"
 		if(fs.existsSync(arquivoDoCurso)){
-
+			this.adicionaTempoAoCurso(arquivoDoCurso, tempoEstudado)
 		}
 		else {
 			this.criarArquivoDeCurso(arquivoDoCurso, {})
-			.then(() => {})
+			.then(() => { this.adicionaTempoAoCurso(arquivoDoCurso, tempoEstudado) })
 		}
 	},
 	criarArquivoDeCurso(nomeArquivo, conteudoDoArquivo){
 		return jsonfile.writeFile(nomeArquivo, conteudoDoArquivo)
 		.then(() => console.log("Arquivo Criado"))
 		.catch(err => console.log(err))
+	},
+	adicionaTempoAoCurso(arquivoDoCurso, tempoEstudado){
+		let dados = {
+			ultimoEstudo: new Date().toString(),
+			tempo: tempoEstudado
+		}
+		jsonfile.writeFile(arquivoDoCurso, dados, {spaces: 2})
+		.then(() => console.log("tempo salvo!"))
+		.catch(err => console.log(err))
+	},
+	pegaDados(curso){
+		let arquivoDoCurso = __dirname + "/data/" + curso + ".json"
+		return jsonfile.readFile(arquivoDoCurso)
 	}
 }
